427c8b46d1ef8ede54ccf0cf34b71a7391bee8bd
Lilieyen <lnyasita@gmail.com>
2021-01-04 20:22:48 +0300

    print x elements of a list using try/except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    list = []
+    list = 0
     for j in range(0, x):
         try:
             print("{:d}".format(my_list[j]), end="")
+            list += 1
         except Exception:
             pass
         print("")
