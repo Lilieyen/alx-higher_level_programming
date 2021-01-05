be2de8811d09fd5557458f763e3ba21f62096ff5
Lilieyen <lnyasita@gmail.com>
2021-01-06 00:02:29 +0300

    fixed do string issue

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defining a square"""
+
+
+class Square:
+    """private instance attribute"""
+    def __init__(self, size=0):
+        """initializing size"""
+        self.size = size
