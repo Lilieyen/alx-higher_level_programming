1fe6d012af744ba42fa0220fa052ee0f132d26c3
Lilieyen <lnyasita@gmail.com>
2021-02-02 10:20:48 +0300

    func to write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """writing to a file"""
-    with open("filename", "w", encoding='utf-8') as myfile:
+    with open("filename", "w", encoding="utf-8") as myfile:
         myfile.write(text)
         return len(text)
