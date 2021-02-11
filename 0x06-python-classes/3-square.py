67d7b682f0f654a3db60190dcd499e6aa49ec9b6
Lilieyen <lnyasita@gmail.com>
2021-02-12 00:17:23 +0300

    area of aquare

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,18 +2,17 @@
 """finding area of class square"""
 
 
-class Square:
+class Square():
     """representing a square"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+            self.__size = size
 
-                def area(self):
-                    """getting area of square"""
-                    return (self.__size) ** 2
+            def area(self):
+                """getting area of square"""
+                return (self.__size) ** 2
