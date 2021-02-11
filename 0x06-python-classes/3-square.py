0b146fd05d3858fc54216de17bd9a8554c02f384
Lilieyen <lnyasita@gmail.com>
2021-02-11 23:50:22 +0300

    getting area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,10 +3,17 @@
 
 
 class Square:
-    """private instance attribute"""
-    def __init__(self, breadth, length):
-        self.breadth = breadth
-        self.length = length
-        """getting area"""
-        def area(self):
-            return self.breadth*self.length
+    """representing a square"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+                def area(self):
+                    """getting area of square"""
+                    return (self.__size) ** 2
