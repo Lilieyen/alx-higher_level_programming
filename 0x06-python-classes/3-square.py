ec5c9e635fad3d9cf2e874aece3dc8de678bc1eb
Lilieyen <lnyasita@gmail.com>
2021-02-12 00:24:30 +0300

    getting area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""finding area of class square"""
+"""
+This file contains the class Square
+"""
 
 
 class Square():
-    """representing a square"""
-
+    """
+    This is the Square class
+    """
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError("size must be an integer")
+            print("size must be an integer", end="")
+            raise TypeError
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            print("size must be >= 0", end="")
+            raise ValueError
         else:
             self.__size = size
 
-            def area(self):
-                """getting area of square"""
-                return (self.__size) ** 2
+    def area(self):
+        """
+        return the area of a square.
+        """
+        return self.__size ** 2
