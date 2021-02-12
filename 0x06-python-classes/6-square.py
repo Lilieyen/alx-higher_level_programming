bd1e4f8f666e893a89ad870f68932fec95daf9a4
Lilieyen <lnyasita@gmail.com>
2021-02-12 17:41:26 +0300

    coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,15 +44,15 @@ class Square():
         if self.__size == 0:
             print("")
 
-    def __init__(self, size=0, position=(0, 0)):
-
-        @property
-        def position(self):
-            return self.__position
+    @property
+    def position(self):
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if isinstance(value, int):
-            if value < 0:
-                print("position must be a tuple of 2 positive integers")
-                raise TypeError
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
