f9805842dcf0e518bf3e43751036f21dfe6a2714
Lilieyen <lnyasita@gmail.com>
2021-03-02 23:25:58 +0300

    script that lists all shows by genres

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,8 +3,8 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 
-SELECT tv_shows.id, tv_show_genres.genre_id,
-FROM tv_showa
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows, tv_show_genres;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
