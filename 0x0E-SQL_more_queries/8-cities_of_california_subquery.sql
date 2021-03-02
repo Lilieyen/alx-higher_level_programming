65160099cb0e88e32793d9cc51943b4bda38aa27
Lilieyen <lnyasita@gmail.com>
2021-03-02 22:24:55 +0300

    lists all cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,5 @@
 -- The states table contains only one record where name = California
 -- Results must be sorted in ascending order by cities.id
 
-SELECT Cities FROM hbtn_0d_usa
-FROM hbtn_0d_usa
-WHERE Cities IN California
-ORDER BY cities.id;
+SELECT id,
+name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
