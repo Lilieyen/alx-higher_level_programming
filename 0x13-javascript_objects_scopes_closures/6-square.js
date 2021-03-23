fd2757a75b20325f3064cdaaefde54a2b2b5ef86
Lilieyen <lnyasita@gmail.com>
2021-03-23 23:57:49 +0300

    inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,8 +6,8 @@ module.exports = class Square extends Square1 {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('c'.repeat(this.width));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
       }
     }
   }
