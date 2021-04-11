dd6a995b2db485bd383b52509803606ca85dcfe1
Lilieyen <lnyasita@gmail.com>
2021-04-11 23:08:30 +0300

    sends a request to the URL and displays the value of a var found in header response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """send a request to the URL & display value of var found in header response"""
-import urllib.request
+import urllib.request as request
 from sys import argv
 
 if __name__ == "__main__":
     req = request.Request(argv[1])
-    with request.urlopen(req) as response:
-        print(response.headers.get('X-Request-Id'))
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
