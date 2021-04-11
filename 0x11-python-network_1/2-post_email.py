74eead6e368709c52b53ec1087d5f544209b3807
Lilieyen <lnyasita@gmail.com>
2021-04-11 23:37:00 +0300

    takes a URL and an email and sends a POST request to the URL with email as a param

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,5 +11,6 @@ import urllib.parse as parse
 if __name__ = "__main__":
     values = {'email': argv[2]}
     data = parse.urlencode(values).encode('utf-8')
-    req = request.urlopen(req) as r:
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as r:
         print(r.read().decode('utf-8'))
