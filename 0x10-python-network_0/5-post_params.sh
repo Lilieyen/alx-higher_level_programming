c467beadf840856c5a201ec4cbfa316f1c3b1234
Lilieyen <lnyasita@gmail.com>
2021-04-11 21:17:04 +0300

    send a POST request with parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL, and has an email an subject var
-curl -sX POST -d {"email: hr@holbertonschool.com"} {"subject: I will always be here for PLD"}
+curl -sX POST -d {"email": "hr@holbertonschool.com", "subject": "I will always be here for PLD"} "$1"
