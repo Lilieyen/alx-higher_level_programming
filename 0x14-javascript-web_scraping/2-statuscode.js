387b1e5fcddf43b0a1bb80abd76e4e03ab9ecd80
Lilieyen <lnyasita@gmail.com>
2021-05-04 21:52:05 +0300

    display status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('request');
+const request = require('request');
 const url = process.argv[2];
-request.get(url, function (err, response) {
+request(url, function (err, response) {
   if (err) {
     console.log(err);
   } else {
